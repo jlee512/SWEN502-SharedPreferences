f1220008f112ae68c321c64968b30dc386baa3ad
Julian Lees <julian.j.lees@gmail.com>
2017-10-16 15:18:54 +1300

    Initial commit

---
+++ app/src/main/java/com/example/julia/activitylifecycleapplication/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.julia.activitylifecycleapplication;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("START", "Application started");
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+
+
+
+            @Override
+            public void onClick(View v) {
+                TextView text = (TextView) findViewById(R.id.changeText);
+                EditText text_input_field = (EditText) findViewById(R.id.editText);
+                String text_input = text_input_field.getText().toString();
+                text.setText(text_input);
+                v.setVisibility(View.GONE);
+                text_input_field.setVisibility(View.GONE);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d("STOPPED", "Application stopped");
+
+//        Get the TextView text value
+        TextView text = (TextView) findViewById(R.id.changeText);
+        String text_to_save = text.getText().toString();
+
+        SharedPreferences sharedPreferences = getSharedPreferences("My Preferences", 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("persist", text_to_save);
+
+//        Commit the changes to the sharedpreferences
+        editor.commit();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d("DESTROYED", "Application destroyed");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d("PAUSE", "Application paused");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("RESUME", "Application resumed");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d("CREATE", "Application created");
+
+        SharedPreferences sharedPreferences = getSharedPreferences("My Preferences", 0);
+        String text = sharedPreferences.getString("persist", null);
+
+        if (text != null) {
+            Log.d("SAVED_STATE", text);
+            TextView text_view = (TextView) findViewById(R.id.changeText);
+            text_view.setText(text);
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.d("RESTART", "Application restarted");
+    }
+}
